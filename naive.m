a3f3b6cc66858b6cb82361e060276e4c06f16b36
Baymax-ray <80106800+Baymax-ray@users.noreply.github.com>
2023-10-17 17:40:43 -0400

    naive model

---
+++ naive.m
@@ -0,0 +1,9 @@
+%% initial parameters
+P_tot=107000;
+P_G1=P_tot*0.137; %<18
+P_G2=P_tot*0.164; %>65
+P_G3=P_tot-P_G1-P_G2; %18-65
+V_P_G1=0; %no one is vaccinated at the beginning
+V_P_G2=0;
+V_P_G3=0;
+
